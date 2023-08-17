b61ff9ebf0856223d01e48fb05ca89010f5c8b2f
brathee2003 <122563832+brathee2003@users.noreply.github.com>
2023-08-17 12:19:39 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ variable "subnet_address_prefixes" {
 }
 
 module "networking" {
-  source  = "app.terraform.io/tfc-workspace-modules/networking/azurerm"
+  source  = "app.terraform.io/bsr-tfc-demo-2023/networking/azurerm"
   version = "~> 1.0"
 
   name                    = var.name
